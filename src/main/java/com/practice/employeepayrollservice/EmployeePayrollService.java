54efa85b281639ef7b18b70ac3290b76a348341d
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-12 18:59:24 +0530

    Welcome message

---
+++ src/main/java/com/practice/employeepayrollservice/EmployeePayrollService.java
@@ -0,0 +1,10 @@
+package com.practice.employeepayrollservice;
+
+public class EmployeePayrollService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to employee payroll service");
+	}
+
+}
